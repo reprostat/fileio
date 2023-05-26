aac3b7613c3f398a54258f2cdcd31ea01fba1405
tiborauer <tibor.auer@gmail.com>
2023-05-26 22:48:55 +0200

    ENH - jsonwrite pretty

--- jsonwrite.m
+++ jsonwrite.m
@@ -8,7 +8,20 @@ function jsonwrite(fname,val)
 %   val   - structure to be saved in the JSON file.
 
     fid = fopen(fname,'w');
-    fwrite(fid,jsonencode(val));
+
+    % Pretty print
+    strJSON = strreps(jsonencode(val),{',' '{' '}' '[' ']'},{',$' '{$' '}$' '[$' '$]$'});
+    strJSON = strrep(strJSON,'$,',',');
+    cellJSON = strsplit(strJSON,'$'); cellJSON(end) = [];
+    nInd = 0;
+    for l = 1:numel(cellJSON)
+        lineJSON{l} = [repmat(' ',1,sum(nInd)) cellJSON{l}];
+        if endsWith(cellJSON{l},{'{' '['}), nInd = [nInd numel(cellJSON{l})]; end
+        if startsWith(cellJSON{l},{'}' ']'}), nInd(end) = []; end
+    end
+
+    % Write
+    fwrite(fid,strjoin(lineJSON,'\n'));
     fclose(fid);
 end
 
